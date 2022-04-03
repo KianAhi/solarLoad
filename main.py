7ad6d15f5a00648b615922beb1f3a64885628fc0
KianAhi <kianahi@hotmail.de>
2022-04-03 19:39:42 +0200

    Added pvgOptions and economicOptions

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
-import sys
-sys.path.append("./src")
 import PySimpleGUI as sg
+from matplotlib.font_manager import json_load
 from radiationGUI import popup_get_date
-import pvgisApi 
+import sys
+import json
+sys.path.append("./src")
 
+from pvgisApi import PVGIS
+
+def readDefaultValues():
+    with open("./data/defaultValues",'r') as f:
+        vars = json.load(f)
+
+    for var in vars['variables'][0]:
+        globals()[var] = vars['variables'][0][var]
 
 if __name__ == "__main__":
-    layout = [ 
-            [sg.Multiline(default_text = "Please Insert Latitude",key="-LAT-" )],
-            [sg.Multiline(default_text = "Please Insert Longitude",key="-LON-" )],
-            [sg.Multiline(default_text = "Please Insert Peak Power",key="-PP-" )],
-            [sg.Multiline(default_text = "Please Insert Energy Cost in cent",key="-ECOST-" )],
-            [sg.Multiline(default_text = "Please Insert Setup Cost",key="-SCOST-" )],
-            [sg.Multiline(default_text = "crystSI",key="-pvTechChoice-" )],
-            [sg.Multiline(default_text = "Please Insert Loss",key="-LOSS-" )],
-            [sg.Multiline(default_text = "Please Insert Latitude",key="-LAT-" )],
-            [sg.B("Start",key="-START-")]
+    #solarEval = PVGIS()
+    readDefaultValues()
+    
+    pvgOptions = [[sg.Text("Latitude"), sg.Multiline(default_text=defaultLat, key="-LAT-", size = (10,1)), 
+                sg.Text("Longitude"), sg.Multiline(default_text = defaultLon, key="-LON-", size = (10,1))],
+                [sg.Frame("PV Technology",[[sg.Radio("crystSi","technology",key='crystSi'),
+                                            sg.Radio("CIS","technology",key='CIS'),
+                                            sg.Radio("CdTe","technology",key='CdTe'),
+                                            sg.Radio("Unknown","technology",key='Unknown')]], border_width=1)],
+                [sg.Text("Peak Power"), sg.Multiline(default_text=defaultPp, key="-PP-", size = (10,1)),
+                sg.Text("Loss"), sg.Multiline(default_text=defaultLoss, key="-LOSS-" ,size=(10,1))  ],
     ]
-    window = sg.Window("Test",layout,finalize=True)
+
+    ecoOptions = [[sg.Text("cent/kWh"),sg.Multiline(default_text=defaultEnergyPrice, key="-ENERGY_PRICE-")]
+
+
+    ]
+    layout = [[sg.Frame("PVG API Options", layout = pvgOptions)]]
+    layout+= [[sg.Frame("Economical Params" ,layout = ecoOptions)]]
+    window = sg.Window("Test", layout, keep_on_top=True, grab_anywhere=True, finalize=True)
+
+    #Update to default Values
+    window[defaultTech].update(True)
 
     while True:
         event, values = window.read()
-        if event == "Close":
+        if event == "Close" or event == "-EXIT-":
             break
         if event == "-START-":
+            pass
             # test = pvgisApi.PVGIS()
             # test.set_value("lat", float(window['-LAT-'].get()))
             # test.set_value("lon", float(window['-LON-'].get()))
@@ -32,4 +53,4 @@ if __name__ == "__main__":
             # test.set_value("optimalangles", 1)
             # test.send_api_request()
             # data = test.get_data(print_output=True)
-            popup_get_date(start_day=1, start_mon=1, start_year=2021, end_day=31, end_month=12, end_year=2021)
+            #popup_get_date(start_day=1, start_mon=1, start_year=2021, end_day=31, end_month=12, end_year=2021)
