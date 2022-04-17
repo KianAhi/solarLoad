973efe6942e95f7d0c2e20c82681d2f84be7a04e
Dario Contrino <dariocontrino@yahoo.de>
2022-04-17 02:03:27 +0200

    added hydrogen storage class

---
+++ src/hydrogenStorage.py
@@ -0,0 +1,28 @@
+class hydrogenStorage:
+    def __init__(self):
+        self.power2H_efficiency = 0.675
+        self.H2power_efficiency = 0.5
+        self.storage_capacity = 5000 # unit: kWh
+        self.stored_power = 0
+        self.effective_output_capacity = 0
+        
+    def input(self, input_power):
+        self.stored_power += input_power * self.power2H_efficiency
+        self.effective_output_capacity = self.stored_power * self.H2power_efficiency
+        if self.stored_power > self.storage_capacity:
+            self.stored_power = self.storage_capacity
+            raise
+    
+    def output(self, output_power):
+        if self.stored_power - (1.0 / self.H2power_efficiency) * output_power > 0: 
+            self.stored_power -= (1.0 / self.H2power_efficiency) * output_power
+            self.effective_output_capacity = self.stored_power * self.H2power_efficiency
+        else:
+            self.stored_power = 0
+            self.effective_output_capacity = 0
+            raise
+    
+    def printIt(self):
+        print("storage capacity = %s" % self.storage_capacity)
+        print("stored power = %s" % self.stored_power)
+        print("eff_output capacity = %s" % self.effective_output_capacity)
