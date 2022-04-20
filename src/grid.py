c84e48f92720c1975f3b327fce9f8dcf04b8faf7
KianAhi <kianahi@hotmail.de>
2022-04-20 18:59:24 +0200

    deleted old comments

--- src/grid.py
+++ src/grid.py
@@ -14,9 +14,6 @@ def model1(houses, hydrogenStorage, startDate = date(2020,1,1), endDate = date(2
     Returns:
         houses, hydrogenStorage
     """
-    # Average hourly data over one year
-    #pvIn
-    #customerUsage
     accumulatorStorage = 0
     months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     hours = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]
